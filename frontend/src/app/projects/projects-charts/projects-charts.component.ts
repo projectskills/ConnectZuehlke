2f4fc58d524fd3a84c23862c14fb491a9df5b51d
Guebeli, Fabian <fabian.guebeli@zuehlke.com>
2019-03-21 17:46:24 +0100

    Add missing chart component

---
+++ frontend/src/app/projects/projects-charts/projects-charts.component.ts
@@ -0,0 +1,18 @@
+import {Component, Input} from '@angular/core';
+import {ChartData} from "../../shared/domain/ChartData";
+
+@Component({
+  selector: 'app-projects-charts',
+  templateUrl: './projects-charts.component.html',
+  styleUrls: ['./projects-charts.component.scss']
+})
+export class ProjectsChartsComponent {
+  @Input() chartData: ChartData[];
+
+  public colorScheme = {
+    domain: ['#5AA454', '#A10A28', '#C7B42C']
+  };
+
+  public view = [700, 200];
+
+}
