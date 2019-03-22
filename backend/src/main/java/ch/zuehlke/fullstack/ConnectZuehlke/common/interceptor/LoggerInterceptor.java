ea83b71c49ec93b9fa8b254fc2b8f73d5398f46e
Guebeli, Fabian <fabian.guebeli@zuehlke.com>
2019-03-22 14:31:19 +0100

    Add logger

---
+++ backend/src/main/java/ch/zuehlke/fullstack/ConnectZuehlke/common/interceptor/LoggerInterceptor.java
@@ -0,0 +1,52 @@
+package ch.zuehlke.fullstack.ConnectZuehlke.common.interceptor;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Enumeration;
+
+public class LoggerInterceptor extends HandlerInterceptorAdapter {
+    private static Logger log = LoggerFactory.getLogger(LoggerInterceptor.class);
+
+    @Override
+    public boolean preHandle(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            Object handler) {
+
+        log.info("[preHandle][" + request + "]" + "[" + request.getMethod()
+                + "]" + request.getRequestURI() + getParameters(request));
+
+        return true;
+    }
+
+    @Override
+    public void postHandle(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            Object handler,
+            ModelAndView modelAndView) {
+
+        log.info("[postHandle][" + request + "]");
+    }
+
+    private String getParameters(HttpServletRequest request) {
+        StringBuilder posted = new StringBuilder();
+        Enumeration<?> e = request.getParameterNames();
+        if (e.hasMoreElements()) {
+            posted.append("?");
+            while (e.hasMoreElements()) {
+                if (posted.length() > 1) {
+                    posted.append("&");
+                }
+                String curr = (String) e.nextElement();
+                posted.append(curr).append("=").append(request.getParameter(curr));
+            }
+        }
+        return posted.toString();
+    }
+}
