fd45189657ac33b1f04c88eb9ca952d1123c0b0e
Suter, Robin <robin.suter@zuehlke.com>
2019-03-22 09:32:27 +0100

    fix backend tests

--- backend/src/main/java/ch/zuehlke/fullstack/ConnectZuehlke/apis/insight/service/InsightSkillServiceMocked.java
+++ backend/src/main/java/ch/zuehlke/fullstack/ConnectZuehlke/apis/insight/service/InsightSkillServiceMocked.java
@@ -4,12 +4,16 @@ import ch.zuehlke.fullstack.ConnectZuehlke.domain.Employee;
 import ch.zuehlke.fullstack.ConnectZuehlke.domain.Project;
 import ch.zuehlke.fullstack.ConnectZuehlke.domain.Skill;
 import ch.zuehlke.fullstack.ConnectZuehlke.domain.SkillExperience;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 import static java.util.Arrays.asList;
 
-public class InsightSkillServcieMocked implements InsightSkillService {
+@Service
+@Profile({"ci", "default"})
+public class InsightSkillServiceMocked implements InsightSkillService {
 
     public static final List<Skill> SKILLS = asList(
             new Skill(1, "Android"),
